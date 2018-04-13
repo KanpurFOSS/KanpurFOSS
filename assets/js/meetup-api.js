80909827e3cb890a3d61322b29ddd5008ccdec19
Hardeep Asrani <me@hardeepasrani.com>
2018-04-14 04:41:05 +0530

    Added "Add to Calendar" button
    Added an "Add to Calendar" button for Google Calendar.

--- assets/js/meetup-api.js
+++ assets/js/meetup-api.js
@@ -22,6 +22,15 @@ function formatDate( date ) {
 	return time + ', ' + weekNames[dayofWeek] + ', ' + day + ', ' + monthNames[monthIndex];
 }
 
+function dateToISO( date ) {
+	return ( new Date( date ) ).toISOString().replace( /-|:|\.\d\d\d/g, '' );
+}
+
+function addToCalendar( event ) {
+	const duration = ( ( event.duration !== undefined ) ? event.duration : 7200000 );
+	const link = `http://www.google.com/calendar/event?action=TEMPLATE&text=${event.name}&details=More+Info:+${event.event_url}&dates=${dateToISO( event.time )}/${dateToISO( event.time + duration )}&location=${event.venue.name + ',+' + event.venue.address_1 + ',+' + event.venue.city}`;
+	return link;
+}
 
 $.ajax({
 	url: 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=15832012%2C15813572%2C15719982%2C23027469%2C25604122%2C26350537&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=132403932&sig=dc12a501ae4c704c31417770c63ad3d09f48bbde',
@@ -51,11 +60,14 @@ $.ajax({
 							<div class="card-media-body-top">
 								<span class="subtle">${formatDate( new Date( event.time ) )}</span>
 								<div class="card-media-body-top-icons u-float-right">
-									<a href="https://www.facebook.com/sharer/sharer.php?u=${event.event_url}" target="_blank">
+									<a href="https://www.facebook.com/sharer/sharer.php?u=${event.event_url}" target="_blank" title="Share on Facebook">
 										<svg class="svg-icon"><use xlink:href="/assets/minima-social-icons.svg#facebook"></use></svg>
 									</a>
-									<a href="https://twitter.com/intent/tweet?text=${event.name + ': ' + event.event_url}&via=KanpurFOSS" target="_blank">
+									<a href="https://twitter.com/intent/tweet?text=${event.name + ': ' + event.event_url}&via=KanpurFOSS" target="_blank" title="Share on Twitter">
 										<svg class="svg-icon"><use xlink:href="/assets/minima-social-icons.svg#twitter"></use></svg>
+									</a>
+									<a href="${addToCalendar( event )}" target="_blank" title="Add to Calendar">
+										<img src="/assets/icons/calendar.svg" />
 									</a>
 								</div>
 							</div>
